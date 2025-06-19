11c3d81f35f61838ae191fea2a1db1b372b6110b
Brijesh <programmercode102@gmail.com>
2025-06-19 12:14:18 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './components/Home/home';
 import Income_Tax_eFiling from './components/Income_Tax_eFiling/income_Tax_eFiling';
 import Book_Consultancy from './components/Book_Consultancy/book_Consultancy';
 import ISO_Certification from './components/ISO_Certification/ISO_Certification';
+import GST_Registration from './components/GST_Registration/GST_Registration';
 
 function Placeholder({ text }) {
   return <div className="p-8 text-center text-xl">{text}</div>;
@@ -22,7 +23,7 @@ function App() {
         {/* Popular Services submenu */}
         <Route path="/popular/service1" element={<Income_Tax_eFiling/>} />
         <Route path="/popular/service2" element={<ISO_Certification/>} />
-        <Route path="/popular/service3" element={<Placeholder text="GST REGISTRATION" />} />
+        <Route path="/popular/service3" element={<GST_Registration/>} />
         <Route path="/popular/service4" element={<Placeholder text="GST RETURN FILING" />} />
         <Route path="/popular/service5" element={<Placeholder text="MSME REGISTRATION" />} />
         <Route path="/popular/service6" element={<Placeholder text="COMPANY REGISTRATION" />} />
