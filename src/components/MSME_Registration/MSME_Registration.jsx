40e813961c6863bd7a254dba4c1013ee740572d0
Brijesh <programmercode102@gmail.com>
2025-06-19 16:21:10 +0530

    first

--- src/components/MSME_Registration/MSME_Registration.jsx
+++ src/components/MSME_Registration/MSME_Registration.jsx
@@ -204,7 +204,10 @@ const MSMEForm = () => {
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
-    setForm((prev) => ({ ...prev, [name]: type === 'checkbox' ? checked : value }));
+    setForm((prev) => ({
+      ...prev,
+      [name]: name === 'pan' ? value.toUpperCase() : (type === 'checkbox' ? checked : value)
+    }));
   };
 
   const handleSubmit = async (e) => {
